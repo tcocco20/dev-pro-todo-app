10248333d9657a5efd82deb9f44e132c609c1465
tcocco20 <theo@coccoscoding.dev>
2024-07-31 22:05:40 -0700

    started styling date time inputs

--- src/components/DateTimeInputs.tsx
+++ src/components/DateTimeInputs.tsx
@@ -2,9 +2,21 @@ import { DatePicker, TimeInput } from "@nextui-org/react";
 
 const DateTimeInputs = () => {
   return (
-    <div className="flex justify-between">
-      <DatePicker label="Select Due Date" labelPlacement="outside" />
-      <TimeInput label="Select Time" labelPlacement="outside" />
+    <div className="flex justify-between gap-5">
+      <DatePicker
+        classNames={{ timeInput: "bg-white py-6", timeInputLabel: "text-lg" }}
+        size="lg"
+        label="Select Due Date"
+        labelPlacement="outside"
+        radius="full"
+      />
+      <TimeInput
+        classNames={{ inputWrapper: "bg-white py-6", label: "text-lg" }}
+        size="lg"
+        label="Select Time"
+        labelPlacement="outside"
+        radius="full"
+      />
     </div>
   );
 };
