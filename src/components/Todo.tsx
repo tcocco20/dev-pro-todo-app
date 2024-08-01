3ae7df05778e9fcc85931dc8b2f256a8d3341dad
tcocco20 <theo@coccoscoding.dev>
2024-08-01 08:36:43 -0700

    went back and finished todo styling

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -6,11 +6,23 @@ import {
   Chip,
   CircularProgress,
 } from "@nextui-org/react";
-import { Check, PenLine } from "lucide-react";
+import { ArrowUp, Calendar, Check, Move, PenLine } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
-export default function Todo() {
+interface TodoProps {
+  urgency: "high" | "medium" | "low";
+}
+
+export default function Todo({ urgency }: TodoProps) {
   const navigate = useNavigate();
+
+  const urgencyIndicatorColor =
+    urgency === "high"
+      ? "red-500"
+      : urgency === "medium"
+      ? "orange-400"
+      : "blue-400";
+
   return (
     <Card
       isPressable
@@ -20,7 +32,7 @@ export default function Todo() {
     >
       <CardHeader className="flex justify-between items-center">
         <div className="flex gap-2 items-center">
-          <div className="w-5 h-5 rounded-full bg-blue-300" />
+          <div className={`w-5 h-5 rounded-full bg-${urgencyIndicatorColor}`} />
           <h3 className="text-xl">Prepare for job interview</h3>
         </div>
         <div className="flex gap-4 items-center">
@@ -39,10 +51,24 @@ export default function Todo() {
         </div>
       </CardHeader>
       <CardBody className="flex flex-row justify-between">
-        <div className="flex flex-col gap-1">
-          <p>Due Date: Tomorrow, 9:00 AM</p>
-          <p>Priority: High (8/10)</p>
-          <p>Complexity: Moderate (5/10)</p>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-2">
+            <Calendar size={20} />
+            <p>
+              <span className="text-zinc-500">Due Date: </span>
+              <span className={`text-${urgencyIndicatorColor}`}>
+                Tomorrow, 9:00 AM
+              </span>
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <ArrowUp size={20} />
+            <p>Priority: High (8/10)</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <Move size={20} />
+            <p>Complexity: Moderate (5/10)</p>
+          </div>
           <div className="flex gap-2 flex-wrap">
             <Chip size="sm" className="bg-teal-200">
               Tag 1
@@ -55,7 +81,7 @@ export default function Todo() {
             </Chip>
           </div>
         </div>
-        <CircularProgress size="lg" value={50} showValueLabel />
+        <CircularProgress size="lg" value={20} showValueLabel />
       </CardBody>
     </Card>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,8 +3,8 @@ import Todo from "./Todo";
 const TodoList = () => {
   return (
     <div className="flex flex-col gap-5">
-      <Todo />
-      <Todo />
+      <Todo urgency="high" />
+      <Todo urgency="low" />
     </div>
   );
 };
